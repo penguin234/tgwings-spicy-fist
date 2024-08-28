915d669f8085d8c77340acd545d133380c5d9697
sjlee-07 <sjlee@sunyoutech.com>
2024-08-28 20:58:23 +0900

    kfold+lgbm+obtuna

--- ai/cong_ai.py
+++ ai/cong_ai.py
@@ -1,21 +1,23 @@
 import joblib
 import pandas as pd
 import math
+import numpy as np
 
-columns = ['1f_1_cong', '1f_but_cong', '2f_he_cong', '2f_2_cong']
+columns = ['1f_1', '1f_but', '2f_he', '2f_2', '1f_ele']
 
 models = {}
 for column in columns:
-    model_filename = "pkl/" + f"{column}_model.pkl"
+    model_filename = "ai/pkl/" + f"{column}_model.pkl"
     model = joblib.load(model_filename)
     models[column] = model
 
 def load_and_predict(future_data, target_column):
     max_seats = {
-    '1f_1_cong': 410,
-    '1f_but_cong': 156,
-    '2f_he_cong': 187,
-    '2f_2_cong': 326
+    '1f_1': 410,
+    '1f_but': 156,
+    '2f_he': 187,
+    '2f_2': 326,
+    '1f_ele' : 47
     }
 
     future_data['datetime'] = pd.to_datetime(future_data['date'] + ' ' + future_data['time'])
@@ -37,12 +39,20 @@ def load_and_predict(future_data, target_column):
     print(f"Congestion rate: {congestion_rate:.2f}%")
 
 
-future_data = pd.DataFrame({
-    'date': ['4/10/2025'],
-    'time': ['18:30'],
+future_data1 = pd.DataFrame({
+    'date': ['6/22/2025'],
+    'time': ['20:30'],
+})
+
+future_data2 = pd.DataFrame({
+    'date': ['3/10/2025'],
+    'time': ['12:30'],
 })
 
 print("\nLoading models and making predictions:")
-for i in range(100):
-    for target_column in ['1f_1_cong', '1f_but_cong', '2f_he_cong', '2f_2_cong']:
-        load_and_predict(future_data, target_column)
+
+for target_column in ['1f_1', '1f_but', '2f_he', '2f_2', '1f_ele']:
+    load_and_predict(future_data1, target_column)
+
+for target_column in ['1f_1', '1f_but', '2f_he', '2f_2', '1f_ele']:
+    load_and_predict(future_data2, target_column)
